979feb3245494c48df3ef21b86ad3569a1094b42
Romaïssa BRAHMI <74942435+Mayy12@users.noreply.github.com>
2024-02-11 11:25:22 +0100

    windguru Date and Hour done

--- windguru.py
+++ windguru.py
@@ -12,32 +12,53 @@ text = ""
 for each in divClass:
     text += each.text
 
+
+# DATE DU JOUR
+indexDay = text.find("hr_d")
+rawDate = text[indexDay+63 : indexDay+85]
+
+Datelist = rawDate.split("\",\"")
+for each in Datelist:
+   each = int(each)
+print(Datelist)
+
+
+# HEURE DU JOUR
+indexHour = text.find("hr_h")
+rawHour = text[indexHour+63 : indexHour+85]
+
+Hourlist = rawHour.split("\",\"")
+for each in Hourlist:
+   each = int(each)
+print(Hourlist)
+
+
+# VITESSE DU VENT
 indexWINDSPD = text.find("WINDSPD")
 rawWINDSPD = text[indexWINDSPD+10 : indexWINDSPD+600]
 
 indexFin = rawWINDSPD.find("]")
 rawWINDSPD = rawWINDSPD[ : indexFin]
-print(rawWINDSPD)
 
 WINDSPDlist = rawWINDSPD.split(",")
-print(WINDSPDlist)
+WINDSPDlist = WINDSPDlist[11:16]
 
-for each in WINDSPDlist :
+for each in WINDSPDlist:
     each = float(each)
 print(WINDSPDlist)
 
 
+
+# DIRECTION DU VENT
 indexWINDDIR = text.find("WINDDIR")
 rawWINDDIR = text[indexWINDDIR+10 : indexWINDDIR+550]
 indexFin = rawWINDDIR.find("]")
 rawWINDDIR = rawWINDDIR[ : indexFin]
 
-print(rawWINDDIR)
-
 WINDDIRlist = rawWINDDIR.split(",")
-print(WINDDIRlist)
+WINDDIRlist = WINDDIRlist[11:16]
 
-for each in WINDDIRlist :
+for each in WINDDIRlist:
     each = float(each)
 print(WINDDIRlist)
 
@@ -46,9 +67,9 @@ print(WINDDIRlist)
 
 
 
-#pour arrondir = (+22.5)
 
-directions = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSO", "SO", "OSO", "O", "ONO", "NO", "NNO"]
+directionNames = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSO", "SO", "OSO", "O", "ONO", "NO", "NNO"]
+directionDegrees = [0.0, 22.5, 45.0, 67.5, 90.0, 112.5, 135.0, 157.5, 180.0, 202.5, 225.0, 247.5, 270.0, 292.5, 315.0, 337.5, 360.0]
 
 
 
