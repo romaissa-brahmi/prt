d15574afbbc9443a638f9ac66dbb621c12b6c4a4
Romaïssa BRAHMI <74942435+Mayy12@users.noreply.github.com>
2024-03-01 09:38:41 +0100

    test serveur n°3 = simple echo date

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import os
 # Vous pouvez placer votre script ici pour recuperer les donnees meteorologiques.
 # Ici à titre d'exemple, la date et l'heure d'execution sont insérées à la suite du fichier "out.txt"
 
-os.system("echo Execution du script par cron : >> sortie.txt")
-os.system("date >> sortie.txt")
+os.system("echo Execution du script par cron : >> standardOutput.txt")
+os.system("date >> standardOutput.txt")
