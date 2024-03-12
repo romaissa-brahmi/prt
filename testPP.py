0924e56958e6bca444828c4a358a0850050d54e3
Romaïssa BRAHMI <74942435+Mayy12@users.noreply.github.com>
2024-03-12 01:16:50 +0100

    test 2334214

--- testPP.py
+++ testPP.py
@@ -33,5 +33,4 @@ if len(today) == 1:
 data = today + "-" + str(datetime.today().month) + " " + str(
     datetime.today().hour) + " " + windSpeed + " " + windDirection
 
-print(data)
 os.system("echo '{}' >> /home/ubuntu/prt/PacificPalissadesData.txt".format(data))
