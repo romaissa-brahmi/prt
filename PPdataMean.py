52cb3f0161559c2ca409a2476c653141fbeb8bda
Romaïssa BRAHMI <74942435+Mayy12@users.noreply.github.com>
2024-03-14 17:14:24 +0100

    TEST 1 : Keep all the data

--- PPdataMean.py
+++ PPdataMean.py
@@ -122,8 +122,7 @@ if len(yesterday) == 1:
     today = "0" + yesterday
 
 
-file = "/home/ubuntu/prt/" + yesterday + "-" + str(datetime.today().month) + "-PacificPalissadesData.txt"
-f = open(file, "r", encoding='utf8')
+f = open("/home/ubuntu/prt/PacificPalissadesData.txt", "r", encoding='utf8')
 data = f.readlines()
 f.close()
 
